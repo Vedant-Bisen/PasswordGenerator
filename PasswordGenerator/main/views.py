ee42bbdc2027a4391c7b8f94a862ea95503edd30
Vedant Bisen <bisenvedant@gmail.com>
2023-02-23 17:27:46 +0530

    main

--- PasswordGenerator/main/views.py
+++ PasswordGenerator/main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Passwords
 
 # Create your views here.
+
+def home(response):
+    return render(response, "main/base.html", {})
+
+def generate(response):
+    pass
