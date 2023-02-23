85fc8f7ec2eed14f9e654aa20a1600eb8d4f2b77
Vedant Bisen <bisenvedant@gmail.com>
2023-02-24 00:05:22 +0530

    main

--- PasswordGenerator/main/forms.py
+++ PasswordGenerator/main/forms.py
@@ -1,10 +1,15 @@
 from django import forms
+from .models import Passwords
 
 
-class CreateNew(forms.Form):
-        username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username', 'style': 'width: 300px;','class':'form-control'}), max_length=100, required=True,)
-        email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Email', 'style': 'width: 300px;', 'class': 'form-control'}),required=False)
-        siteurl = forms.URLField(widget=forms.URLInput(attrs={'placeholder': 'Site URL', 'style': 'width: 300px;', 'class': 'form-control'}), required=True)
-        password = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Password', 'style': 'width: 300px;', 'class': 'form-control'}), max_length=100, required=False)
-        
+class CreateNew(forms.ModelForm):
+    class Meta:
+        model = Passwords
+        fields = ["username", "email", "siteurl", "password"]
 
+        widgets = {
+            "username": forms.TextInput(attrs={"placeholder": "Username","class": "form-control"}),
+            "email": forms.EmailInput(attrs={"placeholder": "Email ID","class": "form-control"}),
+            "siteurl": forms.URLInput(attrs={"placeholder": "Site URL","class": "form-control"}),
+            "password": forms.TextInput(attrs={"placeholder": "Password","class": "form-control"}),
+        }
