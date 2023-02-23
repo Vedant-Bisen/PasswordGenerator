b0d815ac83253878c8121a0eb1dd5bedacf6eece
Vedant Bisen <bisenvedant@gmail.com>
2023-02-23 22:52:31 +0530

    main

--- PasswordGenerator/main/forms.py
+++ PasswordGenerator/main/forms.py
@@ -1,8 +1,10 @@
 from django import forms
 
+
 class CreateNew(forms.Form):
-    username = forms.CharField(label="Name", max_length=100, required=True)
-    email = forms.EmailField(label="Email",required=False)
-    siteurl = forms.URLField(label="Site URL", required=True)
-    password = forms.CharField(label="Password", max_length=100, required=False)
-    
+        username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username', 'style': 'width: 300px;','class':'form-control'}), max_length=100, required=True,)
+        email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Email', 'style': 'width: 300px;', 'class': 'form-control'}),required=False)
+        siteurl = forms.URLField(widget=forms.URLInput(attrs={'placeholder': 'Site URL', 'style': 'width: 300px;', 'class': 'form-control'}), required=True)
+        password = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Password', 'style': 'width: 300px;', 'class': 'form-control'}), max_length=100, required=False)
+        
+
